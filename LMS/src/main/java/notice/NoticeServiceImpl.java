014ab65af0b6be0e1e434ca9815cfb31de15bb5a
wjddmswls11 <wjddmswls10@naver.com>
2022-11-28 15:41:38 +0900

    공지사항

--- LMS/src/main/java/notice/NoticeServiceImpl.java
+++ LMS/src/main/java/notice/NoticeServiceImpl.java
@@ -7,42 +7,35 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NoticeServiceImpl implements NoticeService {
-	@Autowired private NoticeDAO dao;
+	@Autowired
+	private NoticeDAO dao;
+
+	/*
+	 * @Override public NoticePageVO notice_list(NoticePageVO page) {
+	 * 
+	 * return dao.notice_list( page); }
+	 * 
+	 * @Override public NoticeVO notice_info(int id) { return dao.notice_info(id); }
+	 * 
+	 * @Override public int notice_insert(NoticeVO vo) {
+	 * 
+	 * return dao.notice_insert(vo); }
+	 * 
+	 * @Override public int notice_delete(int id) { return dao.notice_delete(id); }
+	 * 
+	 * @Override public int notice_update(NoticeVO vo) { return
+	 * dao.notice_update(vo); }
+	 */
 
 	@Override
-	public NoticePageVO notice_list(NoticePageVO page) {
-	
-		return dao.notice_list( page);
+	public List<NoticeVO> notice_and_list() {
+
+		return dao.notice_and_list();
 	}
 
 	@Override
-	public NoticeVO notice_info(int id) {
-		return dao.notice_info(id);
+	public int notice_delete(NoticeVO vo) {
+		return dao.notice_delete(vo);
 	}
-
-	@Override
-	public int notice_insert(NoticeVO vo) {
-		
-		return dao.notice_insert(vo);
-	}
-
-	@Override
-	public int notice_delete(int id) {
-		return dao.notice_delete(id);
-	}
-
-	@Override
-	public int notice_update(NoticeVO vo) {
-		return dao.notice_update(vo);
-	}
-
-	@Override
-	public int notice_read(int id) {
-		return dao.notice_read(id);
-	}
-
-	
-	
-	
 
 }
